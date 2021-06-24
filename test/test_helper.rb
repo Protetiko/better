527792d13f2951a2e5df236c74a8e4e975a9cdcb
David Sennerlöv <ndavidn@gmail.com>
2021-06-24 15:54:12 +0200

    TestHelper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+
 require "better"
+require 'amazing_print'
 
 require "minitest/autorun"
+
+class MiniTest::Test
+  extend MiniTest::Spec::DSL
+end
